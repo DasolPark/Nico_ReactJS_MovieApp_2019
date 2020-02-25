c4bc17f7222e2ba6154a68677a56e8360c21ee85
Dasol Park <dasol.park615@gmail.com>
2020-02-25 22:42:42 +0900

    3-0. Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const fav = [
-  {
-    id: 1,
-    name: 'kimchi',
-    img:
-      'https://images.unsplash.com/photo-1561505184-ba7ef239577b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    rating: 3.3
-  },
-  {
-    id: 2,
-    name: 'Meat',
-    img:
-      'https://images.unsplash.com/photo-1560781290-7dc94c0f8f4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
-    rating: 4.5
-  },
-  {
-    id: 3,
-    name: 'pasta',
-    img:
-      'https://images.unsplash.com/photo-1551183053-bf91a1d81141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1403&q=80',
-    rating: 2.3
+class App extends React.Component {
+  state = { number: 0 };
+
+  add = () => {
+    console.log('add');
+  };
+
+  sub = () => {
+    console.log('minus');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The Number is {this.state.number}</h1>
+        <button onClick={this.add}>plus</button>
+        <button onClick={this.sub}>minus</button>
+      </div>
+    );
   }
-];
-
-function Food({ name, img, rating }) {
-  return (
-    <div>
-      <h1>I like {name}😍</h1>
-      <h5>rating: {rating}/5.0</h5>
-      <img
-        src={img}
-        alt={name}
-        style={{ width: '250px', height: '250px' }}
-      ></img>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {fav.map(({ name, img, rating, id }) => (
-        <Food name={name} img={img} rating={rating} key={id}></Food>
-      ))}
-    </div>
-  );
 }
 
 export default App;
