ebba2faf859375c5197c87193f673241243f42b4
Dasol Park <dasol.park615@gmail.com>
2020-02-25 13:38:41 +0900

    2-1. Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import Potato from './Potato';
 
-const App = () => {
+function Something({ favFood, booooolean, aaarray }) {
+  return (
+    <div>
+      Something
+      <h1>{favFood}</h1>
+      <h2>{booooolean}</h2>
+      <h3>{aaarray}</h3>
+    </div>
+  );
+}
+
+function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato></Potato>
+      <Something
+        favFood={'kimchi'}
+        booooolean={true}
+        aaarray={[1, 2, 3, 'Hello']}
+      ></Something>
+      <Something
+        favFood={'pasta'}
+        booooolean={false}
+        aaarray={[4, 5, 6, 'Bye']}
+      ></Something>
     </div>
   );
-};
+}
 
 export default App;
