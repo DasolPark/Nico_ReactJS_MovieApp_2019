9be12230f4e3785f831aa5021786968882bcd540
Dasol Park <dasol.park615@gmail.com>
2020-02-25 22:54:54 +0900

    3-1. All you need to know about State

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ class App extends React.Component {
   state = { number: 0 };
 
   add = () => {
-    console.log('add');
+    this.setState(current => ({
+      number: current.number + 1 // Best Case
+    }));
   };
 
   sub = () => {
-    console.log('minus');
+    this.setState({ number: this.state.number - 1 }); // Bad Case
   };
 
   render() {
     return (
       <div>
         <h1>The Number is {this.state.number}</h1>
-        <button onClick={this.add}>plus</button>
-        <button onClick={this.sub}>minus</button>
+        <button onClick={this.add}>
+          <span role="img" aria-label="plus">
+            ➕
+          </span>
+        </button>
+        <button onClick={this.sub}>
+          <span role="img" aria-label="minus">
+            ➖
+          </span>
+        </button>
       </div>
     );
   }
