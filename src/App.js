270d631ae4a2df0dd070d2c202f78eaa5c7f81f1
Dasol Park <dasol.park615@gmail.com>
2020-02-25 13:51:42 +0900

    2-2. Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 
-function Something({ favFood, booooolean, aaarray }) {
+function Something({ name }) {
   return (
     <div>
-      Something
-      <h1>{favFood}</h1>
-      <h2>{booooolean}</h2>
-      <h3>{aaarray}</h3>
+      <h1>I like {name}</h1>
     </div>
   );
 }
 
+const fav = [
+  {
+    name: 'kimchi'
+  },
+  {
+    name: 'Meat'
+  },
+  {
+    name: 'pasta'
+  }
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Something
-        favFood={'kimchi'}
-        booooolean={true}
-        aaarray={[1, 2, 3, 'Hello']}
-      ></Something>
-      <Something
-        favFood={'pasta'}
-        booooolean={false}
-        aaarray={[4, 5, 6, 'Bye']}
-      ></Something>
+      {fav.map(food => (
+        <Something name={food.name}></Something>
+      ))}
     </div>
   );
 }
