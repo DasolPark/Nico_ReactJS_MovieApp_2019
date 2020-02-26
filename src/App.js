3d2514df42d762a25ae061e6ab32dad9b2c4ff3f
Dasol Park <dasol.park615@gmail.com>
2020-02-26 11:26:17 +0900

    4-2. Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
-  state = { isLoading: true, movies: [] };
-
+  state = {
+    isLoading: true,
+    movies: []
+  };
   getMovies = async () => {
     const {
       data: {
@@ -13,7 +16,6 @@ class App extends React.Component {
     } = await axios.get(
       'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
-
     console.log(movies);
     this.setState({ movies, isLoading: false });
   };
@@ -25,22 +27,25 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>
-          {isLoading
-            ? 'Loading...'
-            : movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                ></Movie>
-              ))}
-        </h1>
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
