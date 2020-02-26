d885fa215c886fa6b75132c3f45640430b5d0acf
Dasol Park <dasol.park615@gmail.com>
2020-02-26 11:10:41 +0900

    4-1. Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = { isLoading: true, movies: [] };
 
   getMovies = async () => {
-    const response = await axios.get(
-      'https://yts-proxy.now.sh/list_movies.json'
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
 
-    console.log(response);
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -17,10 +23,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        <h1>{isLoading ? 'Loading...' : 'We are ready!'}</h1>
+        <h1>
+          {isLoading
+            ? 'Loading...'
+            : movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                ></Movie>
+              ))}
+        </h1>
       </div>
     );
   }
